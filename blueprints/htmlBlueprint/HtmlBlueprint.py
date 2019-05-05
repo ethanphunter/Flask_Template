fc1131a06d8850a68a99a4e567d9cddf6d68cbb1
Ethan Hunter <ethanphunter@gmail.com>
2019-05-04 23:34:19 -0500

    Add example of url parameters

--- blueprints/htmlBlueprint/HtmlBlueprint.py
+++ blueprints/htmlBlueprint/HtmlBlueprint.py
@@ -5,3 +5,8 @@ htmlBlueprint = Blueprint("HtmlBlueprint", __name__, template_folder="templates"
 @htmlBlueprint.route("/index", methods = ["GET"])
 def index():
     return render_template("Index.html")
+
+@htmlBlueprint.route("/param")
+def param():
+    p = request.args.get('p')
+    return render_template("Param.html", parameter = p)
