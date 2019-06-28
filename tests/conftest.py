65798b1375f122ed142a6d7af109583cfce08ee9
Ethan Hunter <ethanphunter@gmail.com>
2019-06-27 22:32:22 -0600

    Adding edge case tests and cleaning them up a bit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,18 @@
 import pytest
 import Boot
 from config.Config import Config
+from blueprints.jsonApiBlueprint.library.PostgresLibrary import PostgresLibrary
 
 @pytest.fixture( scope = "module" )
 def client():
     Boot.app.config['TESTING'] = True
     client = Boot.app.test_client()
-
     yield client
 
 @pytest.fixture( scope = "module" )
 def config():
     yield Config()
+
+@pytest.fixture( scope = "module" )
+def postgresLibrary(config):
+    yield PostgresLibrary(config.db_url)
--- tests/test_PostgresLibrary.py
+++ tests/test_PostgresLibrary.py
@@ -1,5 +1,4 @@
 import pytest
-from blueprints.jsonApiBlueprint.library.PostgresLibrary import PostgresLibrary
 from blueprints.jsonApiBlueprint.library.Models import Book
 
 def book():
@@ -8,15 +7,18 @@ def book():
 	    authorId = 1,
 	    bookId = 1235)
 
-def test_addBook(config):
-    pl = PostgresLibrary(config.db_url)
-    assert pl.addBook(book())
+def test_addBook(postgresLibrary):
+    assert postgresLibrary.addBook(book())
+    assert not postgresLibrary.addBook(book())
 
-def test_getAllBooks(config):
-    pl = PostgresLibrary(config.db_url)
-    books = pl.getAllBooks()
+def test_getAllBooks(postgresLibrary):
+    books = postgresLibrary.getAllBooks()
     assert book() in books
 
-def test_deleteBook(config):
-    pl = PostgresLibrary(config.db_url)
-    assert pl.deleteBook(book().bookId)
+def test_deleteBook(postgresLibrary):
+    assert postgresLibrary.deleteBook(book().bookId)
+    assert not postgresLibrary.deleteBook(book().bookId)
+
+def test_getAllBooksRemoved(postgresLibrary):
+    books = postgresLibrary.getAllBooks()
+    assert not book() in books
